c57ee289724e8b5f615deaeed2146670654cc5b3
isaaczs <isaac_zs370@hotmail.com>
2018-10-20 10:51:10 -0500

    Initial commit

---
+++ app/src/main/java/com/zermeno/isaac/actividad_11/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zermeno.isaac.actividad_11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Binding
+        mVideoView = findViewById(R.id.videoview);
+
+        //Set Video View to Media Controller
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(mVideoView);
+
+        //Set Media Controller to Video View
+        mVideoView.setMediaController(controller);
+    }
+
+    private void initializePlayer(){
+        mVideoView.setVideoPath("https://www.ebookfrenzy.com/android_book/movie.mp4");
+        mVideoView.start();
+    }
+
+    private void releasePlayer() {mVideoView.stopPlayback();}
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        releasePlayer();
+    }
+}
